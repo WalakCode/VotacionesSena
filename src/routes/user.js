23db483f616c74d7b5c9c8565f0bccca0166423c
WalakCode <gilhansen96@gmail.com>
2024-02-16 18:26:57 -0500

    oper

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const  {postLogin,getVotos,getMain} = require('../presentation/controller/user.controller')
+const  {postLogin,getVotos,getMain,postMain} = require('../presentation/controller/user.controller')
 const {validarToken} = require('../middlewares/auth.middleware')
 
 router.post('/api/user/login',postLogin)
