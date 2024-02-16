321ea7d27d6634b850eb641727f61ddff902bef5
WalakCode <gilhansen96@gmail.com>
2024-02-16 17:28:41 -0500

    startd added

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const {createPool} = require('mysql2/promise')
 
 const pool = createPool({
-    host:process.env.HOST,
-    user:'root',
-    database:process.env.DATABASE,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    database:process.env.DB_NAME,
 })
 
 module.exports = pool;  
