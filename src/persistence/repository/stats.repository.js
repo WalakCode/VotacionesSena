9df9e0d3c695868e4441ef2e5327fc74c3ac5568
WalakCode <gilhansen96@gmail.com>
2024-02-16 16:48:52 -0500

    si

--- src/persistence/repository/stats.repository.js
+++ src/persistence/repository/stats.repository.js
@@ -2,7 +2,7 @@ const db = require("../../config/db");
 
 const getVotos = async (jornada) => {
   try {
-    const votos = await db.query(`SELECTvt.cedula AS cedula_candidato,j.jornada AS nombre_jornada,COUNT(v.id_votos) AS cantidad_votos
+    const votos = await db.query(`SELECT vt.cedula AS cedula_candidato,j.jornada AS nombre_jornada,COUNT(v.id_votos) AS cantidad_votos
 FROM votos v
 JOIN candidatos c ON v.id_candidato = c.id_candidatos
 JOIN votantes vt ON c.id_votantes_candidatos = vt.id_votantes
